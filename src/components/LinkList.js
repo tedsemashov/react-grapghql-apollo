416925748a6b9444346cb246f94f0a22c4b072dc
Ted Semashov <ted.semashov@gmail.com>
2023-03-09 16:38:20 +0100

    added routing, authentication, caching updates

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,18 +2,28 @@ import React from 'react';
 import Link from './Link';
 import { useQuery, gql } from '@apollo/client';
 
-const FEED_QUERY = gql`
-    {
-        feed {
-            id
-            links {
-                id
-                createdAt
-                url
-                description
-            }
+export const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
         }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
     }
+  }
 `;
 
 const LinkList = () => {
@@ -21,8 +31,8 @@ const LinkList = () => {
 
     return (
         <div>
-            {data?.feed.links.map((link) => (
-                <Link key={link.id} link={link} />
+            {data?.feed.links.map((link, index) => (
+                <Link key={link.id} link={link} index={index} />
             ))}
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 import {
     ApolloProvider,
     ApolloClient,
     createHttpLink,
     InMemoryCache
 } from '@apollo/client';
+import './styles/index.css';
+import App from './components/App';
+import { AUTH_TOKEN } from './constants';
 
 // We create the httpLink that will connect our ApolloClient instance with the GraphQL API.
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
+// This middleware will be invoked every time ApolloClient sends a request to the server.
+// Apollo Links allow us to create middlewares that modify requests before they are sent to the server.
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN);
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    };
+});
+
 // We instantiate ApolloClient by passing in the httpLink and a new instance of an InMemoryCache.
 const client = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <ApolloProvider client={client}>
-    <App />
-    </ApolloProvider>,
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+        <App />
+        </ApolloProvider>
+    </BrowserRouter>,
 );
